6e2b19225cfb1f6cf68a34a3502d62460b76f54f
Lucas Fujiwara <lucastfujiwara@gmail.com>
2021-11-23 19:36:09 -0300

    fix: add placeholder test to profileDataAdapter.test.ts
    Co-authored-by: Ian Galvao <ian.galvao@usp.br>

--- src/app/tests/adapters/profileDataAdapter.test.ts
+++ src/app/tests/adapters/profileDataAdapter.test.ts
@@ -2,5 +2,7 @@ import { ProfileDataAdapter } from '../../adapters/profileDataAdapter';
 
 describe(ProfileDataAdapter, () => {
   // const profileDataAdapter = new ProfileDataAdapter();
-  expect(1 + 1).toStrictEqual(2);
+  test('Placeholder', () => {
+    expect(1 + 1).toStrictEqual(2);
+  });
 });
